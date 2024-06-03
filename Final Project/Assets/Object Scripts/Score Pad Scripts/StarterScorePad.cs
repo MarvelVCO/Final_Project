2dfb2c1e13d79203ec7d62a58d03d2f973be6cad
MarvelVCO <145692524+MarvelVCO@users.noreply.github.com>
2024-06-03 11:02:32 -0400

    Added points, fixing collision issues

--- Final Project/Assets/Object Scripts/Score Pad Scripts/StarterScorePad.cs
+++ Final Project/Assets/Object Scripts/Score Pad Scripts/StarterScorePad.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class StarterScorePad :   MonoBehaviour
 {
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Points.increase(1);
-            Debug.LogError("awdawd");
         }
+        
     }
 }
